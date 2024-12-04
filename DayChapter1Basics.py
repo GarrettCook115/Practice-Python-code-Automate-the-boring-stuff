38509c90fd9fa7b9efde2b03c3d61e5e460e1acd
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2024-12-04 17:57:45 -0500

    Some small edits

--- DayChapter1Basics.py
+++ DayChapter1Basics.py
@@ -9,15 +9,15 @@
 #PEMDAS = (),**,*,/,//,%,+,-
 
 
-print("Hello World") #Basic print statement
-name = input("What is your name?:") # print("What is your name?:" name = input() print("Hello"+ name))
-print("your name length is:")
+print(" Hello World ") #Basic print statement
+name = input(" What is your name?: ") # print("What is your name?:" name = input() print("Hello"+ name))
+print(" your name length is: ")
 leng = len(name)
 print(leng)
 
 age = input("What is your age?:")
-print("You're " + name  +  "and will be the age of " + str(int(age)+1) +"in a year ")
-print("You are" + name + ' and you will be ' + str(int(age) + 1) + ' in a year.') 
+print(" You're " + name  +  " and will be the age of " + str(int(age)+1) +" in a year ")
+print(" You are " + name + ' and you will be ' + str(int(age) + 1) + ' in a year. ') 
 
 # -----------------------------------------------------------------
 # 2+2 #if entered directly into the terminal, it will return 4
@@ -26,10 +26,10 @@ print("You are" + name + ' and you will be ' + str(int(age) + 1) + ' in a year.'
 answer = 43 + 3 #Variable where our store result will be. We can call it anytime. 
 print(answer)
 
-print("Give me" +str(2)) #print the string with the number 2 at the end" If an integer. Encapsulate the number within int(), str(), float(,) etc...
+print(" Give me " +str(2)) #print the string with the number 2 at the end" If an integer. Encapsulate the number within int(), str(), float(,) etc...
 
-print("Give" * 5 ) #printss the string 5 times.(only two numerical values or one string and one integer value as shown.
+print(" Give " * 5 ) #printss the string 5 times.(only two numerical values or one string and one integer value as shown.
 
 print(int(5.5))
 
-print("I can eat "  + str(4) + " sticks") # Converted an int into a string in order to fix an error. or we can use commas "," instead of "+" to separate an it will work aswell without assigning the 4 as a string. . 
+print(" I can eat "  + str(4) + " sticks ") # Converted an int into a string in order to fix an error. or we can use commas "," instead of "+" to separate an it will work aswell without assigning the 4 as a string. . 
