32eb033af8e3f3b1cf0f684620eaf777cf65398b
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2025-03-25 21:27:28 -0400

    Some Edits

--- Chp2Range+Randint.py
+++ Chp2Range+Randint.py
@@ -1,15 +1,17 @@
+import random 
+import random, os, sys, math # example of us now being able to use random,os,sys and math modules if we needed to in the code after. (Can use more than one module)
+
+# on page 120 .
+
 for i in range(1,6):    #prints everything inbetween the two ranges specified. (starting,stop,increment)
     print(i)
 
 for i in range (1,10,2):
     print(i)
 
-import random     #importing moduels to use their features. Random module imported
+    #importing moduels to use their features. Random module imported
 
 for i in range (10):
     print(random.randint(1,90)) # we can now use random.randint() function. Allows us to set a range/ how many times to check inbetween /give us randosm numbers between 1 and 90 10 times. 
                                 #must type "random" infront of the function to utilize its features in the imoprted random module.
 
-import random, os, sys, math # example of us now being able to use random,os,sys and math modules if we needed to in the code after. (Can use more than one module)
-
-# on page 120 .
